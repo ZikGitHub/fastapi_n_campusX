2daddef404a7626a0b2c91a3ef6a353f66dc0940
zishan <zishanenator@gmail.com>
2025-09-13 23:19:58 +0530

    video3

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patient Management System API"}
 
 
 @app.get('/about')
 def about():
-    return {'message': "About Page"} 
+    return {'message': "A Fully functional API to manage your patient records."}
+
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
+
+
+
